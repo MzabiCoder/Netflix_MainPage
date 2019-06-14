b7781904993bbb98c5fca004876f9534e499dc88
Nabil Fannane <nabil@goadroit.com>
2019-06-14 15:15:10 -0700

    test

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 const items = document.querySelectorAll('.tab-item')
-//const contents = document.querySelectorAll('.tab-content-item')
+const contents = document.querySelectorAll('.tab-content-item')
 
 //console.log(items)
 
 function selectItem() {
     removeBord()
+    removeText()
+
+    let ele = document.querySelector(`#${this.id}-content`)
+    ele.classList.add('show')
 
     this.classList.add('tab-border')
 }
@@ -12,4 +16,8 @@ items.forEach(item => item.addEventListener('click', selectItem))
 
 function removeBord() {
     items.forEach(item => item.classList.remove('tab-border'))
+}
+
+function removeText() {
+    contents.forEach(item => item.classList.remove('show'))
 }
